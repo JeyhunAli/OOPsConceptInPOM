4c852b0c3c36c5304e8e356b469a8cee6c214b51
Jey <jey@Jeys-MBP.home>
2020-01-07 22:38:01 -0500

    created one github class

---
+++ src/main/java/com/MyTest/GithubClass.java
@@ -0,0 +1,10 @@
+package com.MyTest;
+
+public class GithubClass {
+
+	public static void main(String[] args) {
+
+		System.out.println("the purpose of this class just to push it to github repo");
+	}
+
+}
